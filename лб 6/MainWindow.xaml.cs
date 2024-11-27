021cef6ab63dd8c44a7999aa4e80287950ae6a1d
SAPR <SAPR@PC-NXT>
2024-11-27 11:09:46 +0700

    def

--- лб 6/MainWindow.xaml.cs
+++ лб 6/MainWindow.xaml.cs
@@ -21,5 +21,13 @@ namespace лб_6 {
         public MainWindow() {
             InitializeComponent();
         }
+
+        private void def_Click(object sender, RoutedEventArgs e) {
+            
+            int zn1 = Convert.ToInt32(z1.Text);
+            int zn2 = Convert.ToInt32(z2.Text);
+            int zn3 = zn1 - zn2;
+            result.Text = zn3.ToString();
+        }
     }
 }
